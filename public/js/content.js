a4350f6397f05d8d08d8b8e9e965801595e6b365
hx2heng <397323988@qq.com>
2017-04-13 01:24:11 +0800

    update

--- public/js/content.js
+++ public/js/content.js
@@ -1,8 +1,8 @@
 import '../css/content.scss';
-
 import './components/nav';
 import './components/share';
 import './components/scrollToTop';
+
 window.onload = () => {
     $(".socialShare").socialShare({
         content: $('.markdown-body').find('p').eq(0).html(),
--- router/tool.js
+++ router/tool.js
@@ -4,14 +4,13 @@ import marked from 'marked'
 let router = express.Router();
 
 
-router.get('/:id', (req, res) => {
+router.get('/', (req, res) => {
     let id = req.query.id;
     if (!id) {
         req.flash('error', '参数错误');
         return res.redirect('back');
     }
     ToolsModel.findToolById(id).then((tool) => {
-        console.log(tool.type);
         if (tool.type === '插件') {
             tool.content = marked(tool.content);
             res.render('content', {
@@ -21,7 +20,7 @@ router.get('/:id', (req, res) => {
                 isConcrete: true
             })
         } else if (tool.type === '在线转换') {
-            res.render('iframe', {
+            res.render('toolContent', {
                 title: tool.title,
                 data: tool
             })
@@ -36,7 +35,11 @@ router.get('/:id', (req, res) => {
 
 })
 
-
-
+//tool-markdown
+router.get('/tool-markdown', (req, res) => {
+    let str = req.query.str.toString();
+    let data = marked(str);
+    return res.json(data)
+})
 
 export default router
